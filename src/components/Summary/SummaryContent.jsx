9a729f0e825fbf4abe3814eebdffa4813c8d1416
Raphael Marques <raphaeljcmarques@gmail.com>
2022-06-13 11:40:02 -0300

    FEAT: Summary API done

--- src/components/Summary/SummaryContent.jsx
+++ src/components/Summary/SummaryContent.jsx
@@ -1,12 +1,12 @@
 import { Separator } from '../Separator';
 import styles from "./summaryContent.module.scss";
 
-export function SummaryContent() {
+export function SummaryContent({ name, transactions }) {
   return (
     <div className={styles.row}>
-      <div>WING20</div>
+      <div>{name}</div>
       <Separator />
-      <span>157 </span>
+      <span>{transactions} </span>
       <span>transações</span>
     </div>
   );
--- src/components/Summary/index.jsx
+++ src/components/Summary/index.jsx
@@ -1,45 +1,64 @@
+import { useEffect, useState } from 'react';
 import commomStyles from '../../styles/common.module.scss';
 import styles from './summary.module.scss';
 import { SummaryContent } from './SummaryContent';
 
 export function Summary() {
+  const [summary, setSummary] = useState();
+
+  useEffect(() => {
+    (async function() {
+      await fetch('https://api-front-test.k8s.smarttbot.com/api/v1/robot/overview')
+      .then(response => response.json())
+      .then(data => setSummary(data.data));
+    })()
+  }, []);
+
   return (
     <section className={`${commomStyles.container} ${styles.summary}`}>
-      <h2>Resumo geral operações</h2>
+      { summary ? (
+        <>
+          <h2>Resumo geral operações</h2>
 
-      <div className={styles.container}>
-        <div>
-          <div className={styles.movement}>
-            <h3>Resumo de Movimentação</h3>
-            <span>-R$220,00</span>
+          <div className={styles.container}>
+            <div>
+              <div className={styles.movement}>
+                <h3>Resumo de Movimentação</h3>
+                <span>{summary.moviment_summary.toLocaleString('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL'
+                })}</span>
+              </div>
+              <div className={styles.total_transactions}>
+                <h3>Total de transações realizadas</h3>
+                <span>{summary.transactions}</span>
+              </div>
+            </div>
+
+            <hr />
+
+            
+            <div className={styles.content}>
+              <h3>Painéis negociados</h3>
+
+              <div>
+                <div className={styles.box}>
+                  { summary.papers.slice(0, 3).map(each => (
+                    <SummaryContent key={each.name} name={each.name} transactions={each.trasactions} />
+                  ))}
+                </div>
+                <div className={styles.box}>
+                  { summary.papers.slice(3).map(each => (
+                    <SummaryContent key={each.name} name={each.name} transactions={each.trasactions} />
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
-          <div className={styles.total_transactions}>
-            <h3>Total de transações realizadas</h3>
-            <span>443</span>
-          </div>
-        </div>
-
-        <hr />
-
-        
-      <div className={styles.content}>
-        <h3>Painéis negociados</h3>
-
-        <div>
-          <div className={styles.box}>
-            <SummaryContent />
-            <SummaryContent />
-            <SummaryContent /> 
-          </div>
-          <div className={styles.box}>
-            <SummaryContent />
-            <SummaryContent />
-            <SummaryContent /> 
-          </div>
-        </div>
-      </div>
-
-      </div>
+        </>
+      ) : (
+        <h2>Carregando dados...</h2>
+      )}
     </section>
   );  
 }
