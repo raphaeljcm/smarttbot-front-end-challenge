2616f63ad86360701bb67880d6616680bad29822
Raphael Marques <raphaeljcmarques@gmail.com>
2022-06-15 08:23:24 -0300

    REFACTOR: Quick update on RobotBox Component

--- src/components/Robot/RobotBox.jsx
+++ src/components/Robot/RobotBox.jsx
@@ -7,7 +7,7 @@ import { ClipLoader } from 'react-spinners';
 
 
 export function RobotBox() {
-  const { robots, addedRobots } = useRobot(); 
+  const { robots } = useRobot(); 
 
   return (
     <>
@@ -49,42 +49,7 @@ export function RobotBox() {
         <>
           <ClipLoader size={25} color='#00B39D' /> <span>Carregando robôs...</span>
         </>
-      )}
-      
-      { addedRobots && 
-        addedRobots.map(robot => (
-          <div key={robot.data.id} className={styles.robot_box}>
-            <header>
-              <h2>{ robot.data.title }</h2>
-              <div>
-                <div className={ robot.data.running === 1 ? `${styles.green}`: `${styles.red}`}></div>
-                <span>{ robot.data.running === 1 ? 'Em execução' : 'Parado' }</span>
-              </div>
-            </header>
-      
-            <p>#{robot.data.id}</p>
-      
-            <div>
-              <RobotLabel value={ robot.data.simulation === 0 ? 'Otimista' : 'Pessimista' } />
-              <RobotLabel value={ robot.data.stock_codes } />
-              <RobotLabel value={ robot.data.type } />
-            </div>
-      
-            { robot.data?.last_paper ? (
-              <RobotContent  
-                position={robot.data.last_paper.position}
-                paper={robot.data.last_paper.paper}
-                type={robot.data.last_paper.type}
-                paper_value={robot.data.last_paper.paper_value}
-                profit={robot.data.last_paper.profit}
-              />
-            ) : (
-              <RobotContent isEmptyData />
-            ) }
-      
-            <RobotFooter daily_balance={robot.data.daily_balance} number_trades={robot.data.number_trades} />
-          </div>
-        ))}
+      )} 
     </>
   );
 }
