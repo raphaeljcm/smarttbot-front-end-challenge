d1edccfae21486d31977e4f99a92319a9de19745
Raphael Marques <raphael.marques@smarttbotter.com>
2023-07-10 09:49:53 -0300

    chore: updating axios baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://api-front-test.k8s.smarttbot.com/api/v1/robot'
+  // baseURL: 'https://api-front-test.k8s.smarttbot.com/api/v1/robot'
+  baseURL: 'https://api.k8s.smarttbot.com/api-front-test/api/v1/robot/'
 });
